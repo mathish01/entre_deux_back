bf5dc9fded8f407eca1cbd912ca67a944a6b6b57
Hanane Benabid <benabid.hanane@gmail.com>
2025-07-23 15:21:22 +0200

    routes posts Done

--- User/Posts/postsRoutes.ts
+++ User/Posts/postsRoutes.ts
@@ -1,39 +1,13 @@
-// routes posts
-// import express, { Request, Response } from 'express'
-// import {PrismaClient} from '@prisma/client';
-
-// const prisma = new PrismaClient();
-// const router = express.Router();
-
-// //route GET
-
-// router.get('/', async (req: Request, res: Response) =>{
-
-//     try{
-//         const posts = await prisma.posts.findMany();
-//         res.json(posts);
-//     } catch(error){
-//         res.status(500).json({error: 'Attention erreur serveur -_-'})
-//     }
-// });
-
 import {Router} from "express"
-import { getPosts, createPosts} from '../../Controler/PostsControler'
+import { getPosts, createPosts, updatePosts, deletePosts} from '../../Controler/PostsControler'
 
 const router = Router()
 
-router.get("/", getPosts)
-router.post ("/", createPosts)
+router.get("/", getPosts);
+router.post ("/", createPosts);
+router.put("/:id", updatePosts);
+router.delete("/:id", deletePosts);
 
 export default router
 
 
-// import { Router } from "express"
-// import { getUsers, createUser } from "../Controler/UserController"
-
-// const router = Router()
-
-// router.get("/", getUsers)
-// router.post("/", createUser)
-
-// export default router
